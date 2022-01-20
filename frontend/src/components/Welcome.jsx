feda3a93e280e13c47e71f055a4308c480307c80
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-01-21 03:48:07 +0800

    modify mint button ;

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,16 +1,34 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { MintContext } from "../context/MintContext";
+import { ethers } from "ethers";
+import abi from '../MyNFT.json'
 const CommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const mintNFT = () =>{
 
-}
 const Welcome = () => {
 
   const { currentAccount, connectWallet} = useContext(MintContext);
 
 
+  const mintNFT = async () =>{
+    const { ethereum } = window;
+
+
+    if(ethereum){
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const signer = provider.getSigner()
+      console.log()
+      console.log(abi.abi)
+      const nftContract = new ethers.Contract("0x92d4da641EaE2bd7a8c1B0E9A9b288c475975fc2",abi.abi,signer)
+      console.log(nftContract)
+      let tx = await nftContract.mintNFT(await signer.getAddress(),"https://gateway.pinata.cloud/ipfs/QmRNkxHiyAM4sL6UR1eT5wcE1fx45pVSAu69o8Q2NVLfde")
+      await tx.wait();
+    }
+    else{
+      alert("Connect to wallet!")
+    }
+  }
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -68,7 +86,7 @@ const Welcome = () => {
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <button
               type="button"
-              onClick={mintNFT("https://gateway.pinata.cloud/ipfs/QmRNkxHiyAM4sL6UR1eT5wcE1fx45pVSAu69o8Q2NVLfde")}
+              onClick={mintNFT}
               className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
             >
               <p className="text-white text-base font-semibold">
