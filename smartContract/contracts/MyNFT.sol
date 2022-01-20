d46c62d2a88a2a3008965d113fc740c78bae42d5
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-01-21 03:46:52 +0800

    modify contract

--- smartContract/contracts/MyNFT.sol
+++ smartContract/contracts/MyNFT.sol
@@ -3,17 +3,15 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract MyNFT is ERC721URIStorage, Ownable {
+contract MyNFT is ERC721URIStorage{
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor() public ERC721("MyNFT", "NFT") {} 
 
-    function mintNFT(address recipient, string memory tokenURI)
-        public onlyOwner
+    function mintNFT(address recipient, string memory tokenURI) public
         returns (uint256)
     {
         _tokenIds.increment();
